37ebade50cb43cd797bc57cea1cef55246478e98
Daniil Abdulov <abdulov.dany@yandex.ru>
2024-11-28 20:39:52 +0500

    fix

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,9 +1,19 @@
 import TeleBot from "telebot"
 
-const bot = new TeleBot('7943845483:AAE1MoK14TFXMiaKP52HnE9ckqMfR76_om0')
+const bot = new TeleBot({
+    token: '7943845483:AAE1MoK14TFXMiaKP52HnE9ckqMfR76_om0',
+    polling: {
+        interval: 1000,
+        timeout: 0,
+        limit: 100,
+        retryTimeout: 5000
+    }
+});
+
+const WELCOME_TEXT = "Добро пожаловать!";
 
 const options = {
-    reply_markup: JSON.stringify({
+    replyMarkup: JSON.stringify({
         inline_keyboard: [
             [
                 { text: 'В тренажер', web_app: { url: 'https://tatar-tele.vercel.app' } },
@@ -15,21 +25,24 @@ const options = {
     })
 };
 
-bot.on('message', (msg) => {
+bot.on('/start', (msg) => {
+    const chatId = msg.chat.id;
+    return bot.sendMessage(chatId, WELCOME_TEXT, options);
+});
+
+bot.on('text', (msg) => {
     const text = msg.text;
     const chatId = msg.chat.id;
-    console.log(msg);
-    if (text === '/start') {
-        return bot.sendMessage(chatId, `${WELCOME_TEXT}`, options);
-    }
+
+    if (text === '/start') return;
 
     return bot.sendMessage(chatId, 'Мин сине анламатым');
 });
 
-bot.on('callback_query', (msg) => {
+bot.on('callbackQuery', (msg) => {
     //
 });
 
-bot.on("polling_error", console.log);
+bot.on("polling_error", (error) => console.log(error));
 
 export default bot
